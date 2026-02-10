777392627850c740e2f551c743fea0454787800e
Kevin Joe Harris <kevinjoeharris1@gmail.com>
2026-02-11 02:49:03 +0530

    Added target groups and listener

--- main.tf
+++ main.tf
@@ -195,19 +195,28 @@ resource "aws_lb" "lb" {
   name               = "lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  security_groups    = [aws_security_group.allow_tls.id]
+  subnets            = []
+}
 
-  enable_deletion_protection = true
+#ALB Target Groups
+resource "aws_lb_target_group" "alb-target-group" {
+  name        = "alb-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.aws-networking-vpc.id
+}
 
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "test-lb"
-    enabled = true
-  }
-
-  tags = {
-    Environment = "production"
+#ALB listner
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb-target-group.arn
   }
 }
+
+
 #Auto-Scaling Groups
