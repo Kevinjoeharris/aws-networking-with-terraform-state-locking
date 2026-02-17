cdcaf918090db60b9f83ca5641840ab72fb236be
Kevin Joe Harris <kevinjoeharris1@gmail.com>
2026-02-18 01:54:13 +0530

    Added variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "availability_zone" {
     type = list 
     default = ["us-east-1a","us-east-1b"]
 }
+
+variable "region" {
+    default = "us-east-1"
+    type = "string"
+    description = "AWS Region"
+}
